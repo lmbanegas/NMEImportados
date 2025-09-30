30ec5fe771ee4b561f853840d629b7546337829a
desarrolloslbanegas <desarrolloslbanegas@gmail.com>
2025-09-29 21:46:33 -0300

    Configuraciones

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-
+const http = require("http");
+const { neon } = require("@neondatabase/serverless");
 const port = process.env.PORT || 9999;
 
 app.get("/", (req, res) => {
@@ -10,3 +11,29 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   `Server started on port ${port}`;
 });
+
+// DB
+
+require("dotenv").config();
+
+
+const sql = neon(process.env.DATABASE_URL);
+
+const requestHandler = async (req, res) => {
+  const result = await sql`SELECT * from public.productos`;
+};
+
+app.get('/hola', function(req, res) {
+  const requestHandler = async (req, res) => {
+    const result = await sql`SELECT * from public.productos`;
+
+    console.log(result)
+    res.send({ result: result.rows })
+
+  }
+
+
+});
+
+
+
