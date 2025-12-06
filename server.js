fdd57852fbe3f3d22854b106b978aa7e561fd40b
Lucas Banegas <131389097+lmbanegas@users.noreply.github.com>
2025-12-06 09:33:44 -0300

    Refactor root route to fetch and serve M3U8 file
    Updated the root route to fetch and return content from a URL instead of redirecting.

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const express = require("express");
 const app = express();
 const http = require("http");
+import fetch from 'node-fetch';
+
 const { neon } = require("@neondatabase/serverless");
 const port = process.env.PORT || 9999;
 
-app.get('/', (req, res) => {
-    res.redirect('https://gist.githubusercontent.com/lmbanegas/2a5c5289bef3cfdbb9179199484981cb/raw/b85c16bac482764c6b83b3ca808ea42e33b5c89a/gistfile1.m3u8');
+
+app.get('/', async (req, res) => {
+  try {
+    const rawUrl =
+      'https://gist.githubusercontent.com/lmbanegas/2a5c5289bef3cfdbb9179199484981cb/raw/b85c16bac482764c6b83b3ca808ea42e33b5c89a/gistfile1.m3u8';
+
+    const response = await fetch(rawUrl);
+    const text = await response.text();
+
+    res.setHeader('Content-Type', 'audio/x-mpegurl');
+    res.send(text);
+  } catch (e) {
+    res.status(500).send('Error cargando lista');
+  }
 });
+
 
 
 app.listen(port, () => {
